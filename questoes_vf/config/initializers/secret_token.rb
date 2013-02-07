9111872f8197209677f7c689ef8c47acc01e699e
Jonathas Dantas <jonathasdantas@gmail.com>
2013-02-07 14:37:59 -0300

    Criacao inicial do projeto

---
+++ questoes_vf/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestoesVf::Application.config.secret_token = '5824c1540fd9e8eb025491a149609726af69579b498fda25533452df21b83f4a221f0fd4a66db138058699a5941cca1abe15f7dbda62c111c2cc91bf1c6737f0'
