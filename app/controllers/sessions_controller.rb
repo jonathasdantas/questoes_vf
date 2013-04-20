2f79a7a5286b5e0c50f3703558af8ee622e72c2f
Jonathas Dantas <jonathasdantas@gmail.com>
2013-04-20 20:00:05 -0300

    teste api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    @user = current_user || User.create_with_omniauth(auth_hash)
+    @user = User.find_by_uid(auth_hash['uid']) ||
+      User.create_with_omniauth(auth_hash)
 
+    Rails.logger.info "Adding #{@user.id} to the session[:user_id]"
     session[:user_id] = @user.uid
+
     redirect_to session[:redirect_to] || inicial_path
+    session[:redirect_to] = nil
   end
 
   protected
