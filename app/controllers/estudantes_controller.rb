10d6d5e2b97531b3f8cd49d5bb568b9ebbe9842e
Jonathas Dantas <jonathasdantas@gmail.com>
2013-04-20 11:30:37 -0300

    ajuste api redu

--- app/controllers/estudantes_controller.rb
+++ app/controllers/estudantes_controller.rb
@@ -11,5 +11,8 @@ class EstudantesController < ApplicationController
   	end
 
   	@provas_grid = initialize_grid(filtro)
+
+    client = ReduClient.new(User.first.access_token, 2957)
+    client.create_canvas(2957, "http://www.redu.com.br", "Meu Vanas")
   end
 end
