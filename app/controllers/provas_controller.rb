846c95341a14320075c584627f4686ffde687685
Jonathas Dantas <jonathasdantas@gmail.com>
2013-04-21 01:11:41 -0300

    teste api

--- app/controllers/provas_controller.rb
+++ app/controllers/provas_controller.rb
@@ -80,7 +80,9 @@ class ProvasController < ApplicationController
 			# Cria o canvas da prova
 			client = client(params[:prova][:disciplina_id])
 			subject = client.create_subject("Prova - " + @prova.titulo)
-			client.create_lecture(subject['id'], @prova.titulo, QuestoesVf::Application.config.root_url + pre_do_test_path(@prova.id, @session_user.uid, :disciplina => params[:prova][:disciplina_id]))
+			logger.info(subject.to_json)
+			logger.info(subject['id'])
+			#client.create_lecture(subject['id'], @prova.titulo, QuestoesVf::Application.config.root_url + pre_do_test_path(@prova.id, @session_user.uid, :disciplina => params[:prova][:disciplina_id]))
 
 			redirect_to index_professores_path(params[:prova][:professor_id], :disciplina => params[:prova][:disciplina_id])
 		else
